59acc660d4b971763950ec7e107a1d5d5583868e
stefvanhouten <stefvanhouten@gmail.com>
2022-05-03 14:48:29 +0200

    Use marshmallow for invoice creation

--- base/pages/invoices.py
+++ base/pages/invoices.py
@@ -4,11 +4,13 @@
 # standard modules
 from itertools import zip_longest
 from marshmallow import Schema, fields, EXCLUDE
+from base.model.invoice import InvoiceProduct
 
 # uweb modules
 import uweb3
-from base import decorators
+from base.decorators import json_error_wrapper
 from base.model import model
+from base.pages.clients import RequestClientSchema
 
 
 class InvoiceSchema(Schema):
@@ -16,22 +18,37 @@ class InvoiceSchema(Schema):
   class Meta:
     unknown = EXCLUDE
 
+  client = fields.Int(required=True, allow_none=False)
   title = fields.Str(required=True, allow_none=False)
   description = fields.Str(required=True, allow_none=False)
-  client = fields.Str(required=True, allow_none=False)
-  status = fields.Str(required=True, allow_none=False)
-  reservation = fields.Str()
+
+
+class ProductSchema(Schema):
+  name = fields.Str(required=True, allow_none=False)
+  price = fields.Decimal(required=True, allow_nan=False)
+  vat_percentage = fields.Int(required=True, allow_none=False)
+  quantity = fields.Int(required=True, allow_none=False)
+
+
+class ProductsCollectionSchema(Schema):
+
+  class Meta:
+    unknown = EXCLUDE
+
+  products = fields.Nested(ProductSchema, many=True, required=True)
 
 
 class PageMaker:
 
   @uweb3.decorators.ContentType('application/json')
-  @decorators.json_error_wrapper
+  @json_error_wrapper
   def RequestInvoices(self):
     return {
         ' invoices': list(model.Invoice.List(self.connection)),
     }
 
+  @uweb3.decorators.ContentType('application/json')
+  @json_error_wrapper
   def RequestInvoiceDetails(self, sequence_number):
     invoice = model.Invoice.FromSequenceNumber(self.connection, sequence_number)
     return {
@@ -40,33 +57,37 @@ class PageMaker:
         'totals': invoice.Totals()
     }
 
+  @uweb3.decorators.ContentType('application/json')
+  @json_error_wrapper
   def RequestNewInvoice(self):
-    client = model.Client.FromClientNumber(self.connection,
-                                           int(self.post.getfirst('client')))
+    client_number = RequestClientSchema().load(dict(self.post))
+    sanitized_invoice = InvoiceSchema().load(dict(self.post))
+    products = ProductsCollectionSchema().load(dict(self.post))
 
-    products = self.post.getlist('products')
-    prices = self.post.getlist('invoice_prices')
-    vat = self.post.getlist('invoice_vat')
-    quantity = self.post.getlist('quantity')
+    client = model.Client.FromPrimary(self.connection, client_number['client'])
+    sanitized_invoice['client'] = client_number['client']
 
-    model.Client.autocommit(self.connection, False)
     try:
-      invoice = model.Invoice.Create(
-          self.connection, {
-              'client': client['ID'],
-              'title': self.post.getfirst('title'),
-              'description': self.post.getfirst('description')
-          })
-      for product, price, vat, quantity in zip_longest(products, prices, vat,
-                                                       quantity):
-        invoice.AddProduct(product, price, vat, int(quantity))
-      model.Client.commit(self.connection)
-    except model.AssemblyError as error:
-      model.Client.rollback(self.connection)
-      return self.Error(error)
+      model.Client.autocommit(self.connection, False)
+      invoice = model.Invoice.Create(self.connection, sanitized_invoice)
+      for product in products['products']:
+        product['invoice'] = invoice['ID']
+        InvoiceProduct.Create(self.connection, product)
+        model.Client.commit(self.connection)
     except Exception as e:
       model.Client.rollback(self.connection)
       raise e
     finally:
       model.Client.autocommit(self.connection, True)
     return self.req.Redirect('/invoices', httpcode=303)
+
+  @uweb3.decorators.TemplateParser('invoice.html')
+  def RequestInvoiceDetails(self, sequence_number):
+    invoice = model.Invoice.FromSequenceNumber(self.connection, sequence_number)
+    companydetails = {'companydetails': self.options.get('companydetails')}
+    invoice.update(companydetails)
+    return {
+        'invoice': invoice,
+        'products': invoice.Products(),
+        'totals': invoice.Totals()
+    }
