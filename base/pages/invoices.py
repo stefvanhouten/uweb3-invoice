c64ddaaf1b8bd44445936b1d912ca29b2e1bfbbc
stefvanhouten <stefvanhouten@gmail.com>
2022-05-04 10:49:41 +0200

    Add regular routes for invoices

--- base/pages/invoices.py
+++ base/pages/invoices.py
@@ -3,6 +3,7 @@
 
 # standard modules
 from itertools import zip_longest
+import requests
 from marshmallow import Schema, fields, EXCLUDE
 from base.model.invoice import InvoiceProduct
 
@@ -11,7 +12,6 @@ import uweb3
 from base.decorators import NotExistsErrorCatcher, json_error_wrapper
 from base.model import model
 from base.pages.clients import RequestClientSchema
-from base import basepages
 
 
 class InvoiceSchema(Schema):
@@ -40,6 +40,75 @@ class ProductsCollectionSchema(Schema):
 
 
 class PageMaker:
+
+  @uweb3.decorators.loggedin
+  @uweb3.decorators.checkxsrf
+  @uweb3.decorators.TemplateParser('invoices/invoices.html')
+  def RequestInvoicesPage(self):
+    return {
+        'invoices': list(model.Invoice.List(self.connection)),
+    }
+
+  @uweb3.decorators.loggedin
+  @uweb3.decorators.checkxsrf
+  @uweb3.decorators.TemplateParser('invoices/create.html')
+  def RequestNewInvoicePage(self):
+    # TODO: Setup for API route nad key.
+    # Catch connectionerror
+    response = requests.get(
+        'http://127.0.0.1:8002/api/v1/products?apikey=6435e79d8b1f6d9ef1cf35fd458bcd5e'
+    )
+    if response.status_code != 200:
+      return self.Error("Something went wrong!")
+
+    return {
+        'clients': list(model.Client.List(self.connection)),
+        'products': response.json()['products'],
+        'scripts': ['/js/invoice.js']
+    }
+
+  @uweb3.decorators.loggedin
+  @uweb3.decorators.checkxsrf
+  def Test(self):
+    products = self.post.getlist('products')
+    prices = self.post.getlist('invoice_prices')
+    vat = self.post.getlist('invoice_vat')
+    quantity = self.post.getlist('quantity')
+
+    product_dict = {'products': []}
+    for product, price, vat, quantity in zip_longest(products, prices, vat,
+                                                     quantity):
+      product_dict['products'].append({
+          'name': product,
+          'price': price,
+          'vat_percentage': vat,
+          'quantity': quantity
+      })
+
+    client = model.Client.FromClientNumber(self.connection,
+                                           int(self.post.getfirst('client')))
+
+    sanitized_invoice = InvoiceSchema().load({
+        'client': client['ID'],
+        'title': self.post.getfirst('title'),
+        'description': self.post.getfirst('description')
+    })
+    products = ProductsCollectionSchema().load(product_dict)
+
+    model.Client.autocommit(self.connection, False)
+    try:
+      invoice = model.Invoice.Create(self.connection, sanitized_invoice)
+      for product in products['products']:
+        product['invoice'] = invoice['ID']
+        # TODO: Add API call to reduce stock
+        InvoiceProduct.Create(self.connection, product)
+      model.Client.commit(self.connection)
+    except Exception as e:
+      model.Client.rollback(self.connection)
+      raise e
+    finally:
+      model.Client.autocommit(self.connection, True)
+    return self.req.Redirect('/invoices', httpcode=303)
 
   @uweb3.decorators.ContentType('application/json')
   @json_error_wrapper
