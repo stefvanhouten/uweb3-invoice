f307158e244f30517fd71abf59719a60e6c7d667
stefvanhouten <stefvanhouten@gmail.com>
2022-05-04 15:20:35 +0200

    Add setup

--- base/basepages.py
+++ base/basepages.py
@@ -25,6 +25,7 @@ class PageMaker(uweb3.DebuggingPageMaker, uweb3.LoginMixin, clients.PageMaker,
     """Sets up all the default vars"""
     self.validatexsrf()
     self.parser.RegisterFunction('CentRound', CentRound)
+    self.parser.RegisterFunction('items', lambda x: x.items())
     self.parser.RegisterFunction('DateOnly', lambda x: str(x)[0:10])
     self.parser.RegisterTag('year', time.strftime('%Y'))
     self.parser.RegisterTag(
@@ -51,7 +52,10 @@ class PageMaker(uweb3.DebuggingPageMaker, uweb3.LoginMixin, clients.PageMaker,
       user = model.Session(self.connection)
     except Exception:
       raise ValueError('Session cookie invalid')
-    user = model.User.FromPrimary(self.connection, int(str(user)))
+    try:
+      user = model.User.FromPrimary(self.connection, int(str(user)))
+    except model.NotExistError:
+      return None
     if user['active'] != 'true':
       raise ValueError('User not active, session invalid')
     return user
@@ -175,27 +179,29 @@ class PageMaker(uweb3.DebuggingPageMaker, uweb3.LoginMixin, clients.PageMaker,
     If these fields are already filled out, this page will not function any
     longer.
     """
-    if self.options.get('general', {}).get('host', False):
-      return self.RequestIndex()
-    if (self.post and 'email' in self.post and 'password' in self.post and
-        'password_confirm' in self.post and 'hostname' in self.post and
-        self.post.getfirst('password')
+    if not model.User.IsFirstUser(self.connection):
+      return self.RequestLogin()
+
+    if ('email' in self.post and 'password' in self.post and
+        'password_confirm' in self.post and self.post.getfirst('password')
         == self.post.getfirst('password_confirm')):
-      user = model.User.Create(
-          self.connection, {
-              'ID': 1,
-              'email': self.post.getfirst('email'),
-              'password': '',
-              'active': 'true'
-          })
       try:
-        user.UpdatePassword(self.post.getfirst('password', ''))
+        user = model.User.Create(self.connection, {
+            'ID': 1,
+            'email': self.post.getfirst('email'),
+            'active': 'true',
+            'password': self.post.getfirst('password'),
+        },
+                                 generate_password_hash=True)
       except ValueError:
         return {'error': 'Password too short, 8 characters minimal.'}
 
-      self.config.Create('general', 'host', self.post.getfirst('hostname'))
-      self.config.Create('general', 'locale',
+      self.config.Update('general', 'host', self.post.getfirst('hostname'))
+      self.config.Update('general', 'locale',
                          self.post.getfirst('locale', 'en_GB'))
+      self.config.Update('general', 'warehouse_api',
+                         self.post.getfirst('warehouse_api'))
+      self.config.Update('general', 'apikey', self.post.getfirst('apikey'))
       model.Session.Create(self.connection, int(user), path="/")
       return self.req.Redirect('/', httpcode=301)
     if self.post:
