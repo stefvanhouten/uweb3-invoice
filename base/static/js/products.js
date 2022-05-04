11882dbfece4100038a75ead72ab3a6584821832
stefvanhouten <stefvanhouten@gmail.com>
2022-05-04 15:20:47 +0200

    Add constants for easier readability

--- base/static/js/products.js
+++ base/static/js/products.js
@@ -52,53 +52,67 @@
 
     update: function (ioEls) {
       // TODO: No hardcoded apikey
-      if(ioEls[0].value != ''){
-          fetch(
-            `http://127.0.0.1:8002/api/v1/search_product/${ioEls[0].value}?apikey=6435e79d8b1f6d9ef1cf35fd458bcd5e`,
-            {
-              mode: "cors",
-              method: "GET",
-            }
-          )
-            .then((response) => {
-              if(response.ok){
-                return response.json()
-              }
-              throw new Error("Something went wrong");
-            })
-            .then((data) => {
-              ioEls[1].value =
-                Number(data["cost"]) + Number(data["assemblycosts"]);
-              ioEls[2].value = Number(data["vat"]);
-
-              var price = ioEls[1].valueAsNumber;
-              var vat = ioEls[2].valueAsNumber;
-              ioEls[6].value = `current: ${data["stock"]} possible: ${data["possible_stock"]}`;
-              let quantity = ioEls[3].valueAsNumber;
-              var vatAmount = ((price * quantity) / 100) * vat;
-              var subtotal = price * quantity + vatAmount;
-
-              if (!isNaN(vat) && !isNaN(vatAmount)) {
-                if (!this.vats[vat]) {
-                  this.vats[vat] = vatAmount;
-                } else {
-                  this.vats[vat] += vatAmount;
-                }
-                ioEls[4].value = "€ " + vatAmount.toFixed(2);
-                ioEls[5].value = "€ " + subtotal.toFixed(2);
-                this.totalEx += price * quantity;
-                this.totalInc += subtotal;
-              } else {
-                ioEls[4].value = "";
-                ioEls[5].value = "";
-              }
-              this.updateVatRows();
-              this.tfootEl.querySelector("tr.totalex output").value =
-                "€ " + this.totalEx.toFixed(2);
-              this.tfootEl.querySelector("tr.totalinc output").value =
-                "€ " + this.totalInc.toFixed(2);
-            });
+      const PRODUCT = 0;
+      const PRICE = 1;
+      const VAT = 2;
+      const QUANTITY = 3;
+      const VAT_AMOUNT = 4;
+      const SUBTOTAL = 5;
+      const STOCK = 6;
+      if (ioEls[PRODUCT].value === "") {
+        ioEls[PRICE].value = "";
+        ioEls[VAT].value = "";
+        ioEls[QUANTITY].value = "";
+        ioEls[STOCK].value = "";
+        return;
       }
+      fetch(
+        `http://127.0.0.1:8002/api/v1/search_product/${ioEls[0].value}?apikey=6435e79d8b1f6d9ef1cf35fd458bcd5e`,
+        {
+          mode: "cors",
+          method: "GET",
+        }
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("Something went wrong");
+        })
+        .then((data) => {
+          ioEls[PRICE].value =
+            Number(data["cost"]) + Number(data["assemblycosts"]);
+          ioEls[VAT].value = Number(data["vat"]);
+
+          var price = ioEls[1].valueAsNumber;
+          var vat = ioEls[2].valueAsNumber;
+          ioEls[
+            STOCK
+          ].value = `current: ${data["stock"]} possible: +${data["possible_stock"]}`;
+          let quantity = ioEls[3].valueAsNumber;
+          var vatAmount = ((price * quantity) / 100) * vat;
+          var subtotal = price * quantity + vatAmount;
+
+          if (!isNaN(vat) && !isNaN(vatAmount)) {
+            if (!this.vats[vat]) {
+              this.vats[vat] = vatAmount;
+            } else {
+              this.vats[vat] += vatAmount;
+            }
+            ioEls[VAT_AMOUNT].value = "€ " + vatAmount.toFixed(2);
+            ioEls[SUBTOTAL].value = "€ " + subtotal.toFixed(2);
+            this.totalEx += price * quantity;
+            this.totalInc += subtotal;
+          } else {
+            ioEls[VAT_AMOUNT].value = "";
+            ioEls[SUBTOTAL].value = "";
+          }
+          this.updateVatRows();
+          this.tfootEl.querySelector("tr.totalex output").value =
+            "€ " + this.totalEx.toFixed(2);
+          this.tfootEl.querySelector("tr.totalinc output").value =
+            "€ " + this.totalInc.toFixed(2);
+        });
     },
 
     updateVatRows: function () {
