3fe83f75a568acbbfb5f581823760f59e70152dd
stefvanhouten <stefvanhouten@gmail.com>
2022-05-16 15:03:47 +0200

    Manually update bugged merge page

--- invoices/base/pages/invoices.py
+++ invoices/base/pages/invoices.py
@@ -2,19 +2,14 @@
 """Request handlers for the uWeb3 warehouse inventory software"""
 
 # standard modules
-from http.client import FORBIDDEN
-import re
-import mt940
 import decimal
 import requests
-import marshmallow
+from marshmallow.exceptions import ValidationError
 from http import HTTPStatus
-from itertools import zip_longest
-from marshmallow import Schema, fields, EXCLUDE, post_load, validate
-from invoices.base.model.invoice import InvoiceProduct
-
-from weasyprint import HTML
-from io import BytesIO, StringIO
+from invoices.base.pages.helpers.invoices import *
+from invoices.base.pages.helpers.general import transaction
+from invoices.base.pages.helpers.invoices import InvoicePair
+from invoices.base.pages.helpers.schemas import InvoiceSchema, ProductsCollectionSchema
 
 # uweb modules
 import uweb3
@@ -25,221 +20,21 @@ from invoices.base.decorators import NotExistsErrorCatcher, RequestWrapper
 INVOICE_REGEX_PATTERN = r"([0-9]{4}-[0-9]{3})|(PF-[0-9]{4}-[0-9]{3})"
 
 
-def ToPDF(html, filename=None):
-  """Returns a PDF based on the given HTML."""
-  result = BytesIO()
-  HTML(string=html).write_pdf(result)
-  if filename:
-    result.filename = filename
-    return result
-  return result.getvalue()
-
-
-def CreateCleanProductList(products, negative_abs=False):
-  """Create a simple list containing {name: value, quantity: x} pairs.
-
-  Arguments:
-    % negative_abs: Change the quantity to an absolute value or leave as is.
-                    This is used when adding stock or decrementing stock.
-  """
-  items = []
-  for product in products:
-    # Check if a product was entered multiple times, if so update quantity of said product.
-    target = list(filter(lambda item: item['name'] == product['name'], items))
-    if len(target) > 0:
-      target[0]['quantity'] += -abs(
-          product['quantity']) if negative_abs else product['quantity']
-      continue
-    # If product not yet in items, add it.
-    items.append({
-        'name':
-            product['name'],
-        'quantity':
-            -abs(product['quantity']) if negative_abs else product['quantity']
-    })
-  return items
-
-
-def regex_search(file, regex):
-  """Parse a StringIO object"""
-  transactions = mt940.models.Transactions(processors=dict(pre_statement=[
-      mt940.processors.add_currency_pre_processor('EUR'),
-  ],))
-  data = file.read()
-  transactions.parse(data)
-  results = []
-  for transaction in transactions:
-    matches = re.finditer(regex, transaction.data['transaction_details'],
-                          re.MULTILINE)
-    amount = str(transaction.data['amount'].amount)
-    results.extend([{"invoice": x.group(), "amount": amount} for x in matches])
-  return results
-
-
-def get_and_zip_products(products, product_prices, product_vat,
-                         product_quantity):
-  """Transform invoice products post data to a list of dictionaries.
-  This function uses zip_longest, so any missing data will be filled with None.
-
-  Arguments:
-    @ products: list
-    @ product_prices: list
-    @ product_vat: list
-    @ product_quantity: list
-
-  Returns: [
-      { name: The name of the product,
-        price: The price of the product,
-        vat_percentage: specified vat percentage of a given product,
-        quantity: The amount of products that were specified
-      }]
-  """
-  products = []
-  for product, price, vat, quantity in zip_longest(products, product_prices,
-                                                   product_vat,
-                                                   product_quantity):
-    products.append({
-        'name': product,
-        'price': price,
-        'vat_percentage': vat,
-        'quantity': quantity
-    })
-  return products
-
-
 class WarehouseAPIException(Exception):
   """Error that was raised during an API call to warehouse."""
 
 
-class InvoiceSchema(Schema):
+class PageMaker:
 
-  class Meta:
-    unknown = EXCLUDE
-
-  client = fields.Int(required=True, allow_none=False)
-  title = fields.Str(required=True, allow_none=False)
-  description = fields.Str(required=True, allow_none=False)
-  status = fields.Str(
-      missing='new')  # Default status to new when field is missing.
-
-  @post_load
-  def no_status(self, item, *args, **kwargs):
-    """When an empty string is provided set status to new."""
-    if not item['status'] or item['status'] == '':
-      item['status'] = 'new'
-    if item['status'] == 'on':  # This is for when the checkbox value is passed.
-      item['status'] = 'reservation'
-    return item
-
-
-class ProductSchema(Schema):
-  name = fields.Str(required=True, allow_none=False)
-  price = fields.Decimal(required=True, allow_nan=False)
-  vat_percentage = fields.Int(required=True, allow_none=False)
-  quantity = fields.Int(required=True, allow_none=False)
-
-
-class ProductsCollectionSchema(Schema):
-
-  class Meta:
-    unknown = EXCLUDE
-
-  products = fields.Nested(ProductSchema, many=True, required=True)
-
-
-class CompanyDetailsSchema(Schema):
-
-  class Meta:
-    unknown = EXCLUDE
-
-  name = fields.Str(required=True, allow_none=False)
-  telephone = fields.Str(required=True, allow_none=False)
-  address = fields.Str(required=True, allow_none=False)
-  postalCode = fields.Str(
-      required=True,
-      allow_none=False,
-      validate=validate.Regexp(
-          r"^[1-9][0-9]{3} ?(?!sa|sd|ss|SA|SD|SS)[A-Za-z]{2}$",
-          error="Should be 4 numbers and 2 letters"))
-  city = fields.Str(required=True, allow_none=False)
-  country = fields.Str(required=True, allow_none=False)
-  vat = fields.Str(required=True, allow_none=False)
-  kvk = fields.Str(required=True, allow_none=False)
-  bankAccount = fields.Str(required=True, allow_none=False)
-  bank = fields.Str(required=True, allow_none=False)
-  bankCity = fields.Str(required=True, allow_none=False)
-  invoiceprefix = fields.Str(required=True, allow_none=False)
-
-
-class APIPages:
-  # @uweb3.decorators.ContentType('application/json')
-  # @json_error_wrapper
-  # def RequestInvoices(self):
-  #   return {
-  #       ' invoices': list(model.Invoice.List(self.connection)),
-  #   }
-
-  # @uweb3.decorators.ContentType('application/json')
-  # @json_error_wrapper
-  # def RequestNewInvoice(self):
-  #   client_number = RequestClientSchema().load(dict(self.post))
-  #   sanitized_invoice = InvoiceSchema().load(dict(self.post))
-  #   products = ProductsCollectionSchema().load(dict(self.post))
-
-  #   client = model.Client.FromPrimary(self.connection, client_number['client'])
-  #   sanitized_invoice['client'] = client['ID']
-
-  #   invoice = self._handle_create(sanitized_invoice, products['products'])
-  #   return self.RequestInvoiceDetailsJSON(invoice['sequenceNumber'])
-
-  # @uweb3.decorators.ContentType('application/json')
-  # @json_error_wrapper
-  # def RequestInvoiceDetailsJSON(self, sequence_number):
-  #   invoice = model.Invoice.FromSequenceNumber(self.connection, sequence_number)
-  #   companydetails = {'companydetails': self.options.get('companydetails')}
-  #   invoice.update(companydetails)
-  #   return {
-  #       'invoice': invoice,
-  #       'products': list(invoice.Products()),
-  #       'totals': invoice.Totals()
-  #   }
-
-  def _handle_create(self, sanitized_invoice, products):
-    api_url = self.config.options['general']['warehouse_api']
-    api_key = self.config.options['general']['apikey']
-
-    items = CreateCleanProductList(products, negative_abs=True)
-
-    try:
-      model.Client.autocommit(self.connection, False)
-      invoice = model.Invoice.Create(self.connection, sanitized_invoice)
-      for product in products:
-        product['invoice'] = invoice['ID']
-        InvoiceProduct.Create(self.connection, product)
-
-      reference = f"Buy order for invoice: {invoice['sequenceNumber']}"
-      if invoice['status'] == 'reservation':
-        reference = f"Reservation for invoice: {invoice['sequenceNumber']}"
-      response = requests.post(f'{api_url}/products/bulk_stock',
-                               json={
-                                   "apikey": api_key,
-                                   "products": items,
-                                   "reference": reference,
-                               })
-      if response.status_code == 200:
-        model.Client.commit(self.connection)
-      else:
-        model.Client.rollback(self.connection)
-        raise WarehouseAPIException(response.json())
-    except Exception:
-      model.Client.rollback(self.connection)
-      raise
-    finally:
-      model.Client.autocommit(self.connection, True)
-    return invoice
-
-
-class PageMaker(APIPages):
+  def _get_warehouse_api_data(
+      self
+  ):  # XXX: This is used because the __init__ and _PostInit methods are not called because this PageMakers super class is object.
+    """Reads the config for warehouse API data and sets:
+    - self.warehouse_api_url: The warehouse url to access the API
+    - self.warehouse_apikey: The apikey that is needed to access the warehouse API
+    """
+    self.warehouse_api_url = self.config.options['general']['warehouse_api']
+    self.warehouse_apikey = self.config.options['general']['apikey']
 
   @uweb3.decorators.loggedin
   @uweb3.decorators.checkxsrf
@@ -255,28 +50,30 @@ class PageMaker(APIPages):
   @RequestWrapper
   @uweb3.decorators.TemplateParser('invoices/create.html')
   def RequestNewInvoicePage(self, errors=[]):
-    api_url = self.config.options['general']['warehouse_api']
-    apikey = self.config.options['general']['apikey']
-
-    response = requests.get(f'{api_url}/products?apikey={apikey}')
+    self._get_warehouse_api_data()
+    response = requests.get(
+        f'{self.warehouse_api_url}/products?apikey={self.warehouse_apikey}')
 
     if response.status_code != 200:
-      return self._hadle_api_status_error(response)
+      return self._handle_api_status_error(response)
 
     json_response = response.json()
     return {
         'clients': list(model.Client.List(self.connection)),
         'products': json_response['products'],
         'errors': errors,
-        'api_url': api_url,
-        'apikey': apikey,
+        'api_url': self.warehouse_api_url,
+        'apikey': self.warehouse_apikey,
         'scripts': ['/js/invoice.js']
     }
 
-  def _hadle_api_status_error(self, response):
+  def _handle_api_status_error(self, response):
     json_response = response.json()
+
     if response.status_code == HTTPStatus.NOT_FOUND:
-      return self.Error(f"Warehouse API at url '{api_url}' could not be found.")
+      return self.Error(
+          f"Warehouse API at url '{self.warehouse_api_url}' could not be found."
+      )
     elif response.status_code == HTTPStatus.FORBIDDEN:
       error = json_response.get(
           'error',
@@ -286,6 +83,7 @@ class PageMaker(APIPages):
 
   @uweb3.decorators.loggedin
   @uweb3.decorators.checkxsrf
+  @RequestWrapper
   def RequestCreateNewInvoicePage(self):
     # TODO: Handle validation errors
     products = get_and_zip_products(self.post.getlist('products'),
@@ -302,18 +100,45 @@ class PageMaker(APIPages):
           'status': self.post.getfirst('reservation', '')
       })
       products = ProductsCollectionSchema().load({'products': products})
-      invoice = self._handle_create(sanitized_invoice, products['products'])
-    except marshmallow.exceptions.ValidationError as error:
+    except ValidationError as error:
       return self.RequestNewInvoicePage(errors=[error.messages])
-    except WarehouseAPIException as error:
-      if 'errors' in error.args[0]:
-        return self.RequestNewInvoicePage(errors=error.args[0]['errors'])
-      return self.RequestNewInvoicePage(errors='Something went wrong')
 
-    if invoice:
+    self._get_warehouse_api_data(
+    )  # Make sure that self.warehouse_apikey and self.warehouse_url are set.
+    try:
+      invoice = self._create_invoice_and_products(sanitized_invoice,
+                                                  products['products'])
+    except WarehouseAPIException as error:
+      return self.RequestNewInvoicePage(errors=error.args[0]['errors'])
+
+    if invoice and self.post.getfirst('shouldmail'):
       self.mail_invoice(invoice,
                         self.RequestInvoiceDetails(invoice['sequenceNumber']))
     return self.req.Redirect('/invoices', httpcode=303)
+
+  def _create_invoice_and_products(self, sanitized_invoice, invoice_products):
+    clean_products = CreateCleanProductList(invoice_products, negative_abs=True)
+
+    with transaction(self.connection, model.Invoice):
+      invoice = model.Invoice.Create(self.connection, sanitized_invoice)
+      invoice.AddProducts(invoice_products)
+      reference_message = decide_reference_message(invoice['status'],
+                                                   invoice['sequenceNumber'])
+
+      response = requests.post(f'{self.warehouse_api_url}/products/bulk_stock',
+                               json={
+                                   "apikey": self.warehouse_apikey,
+                                   "products": clean_products,
+                                   "reference": reference_message,
+                               })
+      if response.status_code != 200:
+        model.Client.rollback(self.connection)
+        json_response = response.json()
+        if 'errors' in json_response:
+          raise WarehouseAPIException(json_response['errors'])
+        return self._handle_api_status_error(response)
+
+    return invoice
 
   @uweb3.decorators.TemplateParser('invoices/invoice.html')
   @NotExistsErrorCatcher
@@ -366,8 +191,7 @@ class PageMaker(APIPages):
   @NotExistsErrorCatcher
   def RequestInvoiceCancel(self):
     """Sets the given invoice to paid."""
-    api_url = self.config.options['general']['warehouse_api']
-    apikey = self.config.options['general']['apikey']
+    self._get_warehouse_api_data()
 
     invoice = self.post.getfirst('invoice')
     invoice = model.Invoice.FromSequenceNumber(self.connection, invoice)
@@ -375,15 +199,16 @@ class PageMaker(APIPages):
 
     items = CreateCleanProductList(products)
     response = requests.post(
-        f'{api_url}/products/bulk_stock',
+        f'{self.warehouse_api_url}/products/bulk_stock',
         json={
-            "apikey": apikey,
+            "apikey": self.warehouse_apikey,
             "reference": f"Canceling reservation: {invoice['sequenceNumber']}",
             "products": items
         })
-    if response.status_code == 200:
-      invoice['status'] = 'canceled'
-      invoice.Save()
+    if response.status_code != 200:
+      return self._handle_api_status_error(response)
+
+    invoice.CancelProFormaInvoice()
     return self.req.Redirect('/invoices', httpcode=303)
 
   def mail_invoice(self, invoice, details):
@@ -414,33 +239,22 @@ class PageMaker(APIPages):
   @uweb3.decorators.checkxsrf
   def RequestUploadMt940(self):
     # TODO: File validation.
-    changed_invoices = []
-    failed_invoices = []
-    for posted_file in self.files.get('fileupload', []):
-      io_file = StringIO(posted_file['content'])
-      results = regex_search(io_file, INVOICE_REGEX_PATTERN)
-      for res in results:
-        try:
-          invoice = model.Invoice.FromSequenceNumber(self.connection,
-                                                     res['invoice'])
-          price = invoice.Totals()['total_price']
-          res['amount'] = decimal.Decimal(res['amount'])
-          if res['amount'] == price:
-            previous_status = invoice['status']
-            invoice.SetPayed()
-            invoice['previous_status'] = previous_status
-            changed_invoices.append(invoice)
-          else:
-            # XXX: These fields do not exist on a real invoice object. This is purely for the failed invoices table.
-            invoice['actual_amount'] = price
-            invoice['expected_amount'] = res['amount']
-            invoice['diff'] = res['amount'] - price
-            failed_invoices.append(invoice)
-        except (uweb3.model.NotExistError, Exception) as e:
-          # Invoice could not be found. This could mean two things,
-          # 1. The regex matched something that looks like an invoice sequence number, but its not part of our system.
-          # 2. The transaction contains a pro-forma invoice, but this invoice was already set to paid and thus changed to a real invoice.
-          # its also possible that there was a duplicate pro-forma invoice ID in the description, but since it was already processed no reference can be found to it anymore.
-          continue
-    return self.RequestMt940(changed_invoices=changed_invoices,
-                             failed_invoices=failed_invoices)
+    pairs = []
+    found_invoice_references = MT940_processor(self.files.get(
+        'fileupload', [])).process_files()
+    for invoice_ref in found_invoice_references:
+      try:
+        invoice = model.Invoice.FromSequenceNumber(self.connection,
+                                                   invoice_ref['invoice'])
+        pairs.append(InvoicePair(invoice, invoice_ref))
+      except (uweb3.model.NotExistError, Exception) as e:
+        # Invoice could not be found. This could mean two things,
+        # 1. The regex matched something that looks like an invoice sequence number, but its not part of our system.
+        # 2. The transaction contains a pro-forma invoice, but this invoice was already set to paid and thus changed to a real invoice.
+        # its also possible that there was a duplicate pro-forma invoice ID in the description, but since it was already processed no reference can be found to it anymore.
+        continue
+
+    handler = MT940_invoice_handler(pairs)
+    handler.process()
+    return self.RequestMt940(changed_invoices=handler.processed_invoices,
+                             failed_invoices=handler.failed_invoices)
