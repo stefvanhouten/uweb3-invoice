9c73445a02180f988255056cb20eb4c00bb9e857
stefvanhouten <stefvanhouten@gmail.com>
2022-05-16 12:10:09 +0200

    Use enum for statuses. Make sure only pro forma invoices can be canceled.

--- tests/test_invoice_model.py
+++ tests/test_invoice_model.py
@@ -1,6 +1,7 @@
 import datetime
 from datetime import date
 from decimal import Decimal
+from multiprocessing.sharedctypes import Value
 import pytest
 
 from invoices.base.model import invoice, model
@@ -87,7 +88,7 @@ def simple_invoice_dict(client_object, companydetails_object) -> dict:
 @pytest.fixture
 def create_invoice_object(connection, client_object, companydetails_object):
 
-  def create(status='new') -> invoice.Invoice:
+  def create(status=invoice.InvoiceStatus.NEW.value) -> invoice.Invoice:
     return invoice.Invoice.Create(
         connection, {
             'title': 'test invoice',
@@ -162,10 +163,13 @@ class TestClass:
   def test_invoice_and_pro_forma_mix_sequence_number(self, connection,
                                                      client_object,
                                                      create_invoice_object):
-    real_invoice = create_invoice_object(status='new')
-    pro_forma = create_invoice_object(status='reservation')
-    second_real_invoice = create_invoice_object(status='new')
-    second_pro_forma = create_invoice_object(status='reservation')
+    real_invoice = create_invoice_object(status=invoice.InvoiceStatus.NEW.value)
+    pro_forma = create_invoice_object(
+        status=invoice.InvoiceStatus.RESERVATION.value)
+    second_real_invoice = create_invoice_object(
+        status=invoice.InvoiceStatus.NEW.value)
+    second_pro_forma = create_invoice_object(
+        status=invoice.InvoiceStatus.RESERVATION.value)
 
     assert real_invoice['sequenceNumber'] == f'{date.today().year}-001'
     assert pro_forma[
@@ -178,32 +182,50 @@ class TestClass:
     assert calc_due_date() == invoice.Invoice.CalculateDateDue()
 
   def test_pro_forma_to_real_invoice(self, create_invoice_object):
-    pro_forma = create_invoice_object(status='reservation')
-    assert pro_forma['status'] == 'reservation'
+    pro_forma = create_invoice_object(
+        status=invoice.InvoiceStatus.RESERVATION.value)
+    assert pro_forma['status'] == invoice.InvoiceStatus.RESERVATION
 
     pro_forma.ProFormaToRealInvoice()
 
-    assert pro_forma['status'] == 'new'
+    assert pro_forma['status'] == invoice.InvoiceStatus.NEW
     assert pro_forma['dateDue'] == calc_due_date()
 
   def test_invoice_to_paid(self, create_invoice_object):
-    inv = create_invoice_object(status='new')
-    assert inv['status'] == 'new'
+    inv = create_invoice_object(status=invoice.InvoiceStatus.NEW.value)
+    assert inv['status'] == invoice.InvoiceStatus.NEW
 
     inv.SetPayed()
 
-    assert inv['status'] == 'paid'
+    assert inv['status'] == invoice.InvoiceStatus.PAID
     assert inv['dateDue'] == calc_due_date()
 
   def test_pro_forma_to_paid(self, create_invoice_object):
-    pro_forma = create_invoice_object(status='reservation')
-    assert pro_forma['status'] == 'reservation'
+    pro_forma = create_invoice_object(
+        status=invoice.InvoiceStatus.RESERVATION.value)
+    assert pro_forma['status'] == invoice.InvoiceStatus.RESERVATION
 
     pro_forma.SetPayed()
 
     assert pro_forma['sequenceNumber'] == f'{date.today().year}-001'
-    assert pro_forma['status'] == 'paid'
+    assert pro_forma['status'] == invoice.InvoiceStatus.PAID
     assert pro_forma['dateDue'] == calc_due_date()
+
+  def test_pro_forma_to_canceled(self, create_invoice_object):
+    pro_forma = create_invoice_object(
+        status=invoice.InvoiceStatus.RESERVATION.value)
+    pro_forma.CancelProFormaInvoice()
+
+    assert pro_forma['status'] == invoice.InvoiceStatus.CANCELED
+    # Make sure the sequenceNumber is still a pro forma sequenceNumber
+    assert pro_forma[
+        'sequenceNumber'] == f'{invoice.PRO_FORMA_PREFIX}-{date.today().year}-001'
+
+  def test_real_invoice_to_canceled(self, create_invoice_object):
+    inv = create_invoice_object(status=invoice.InvoiceStatus.NEW.value)
+    with pytest.raises(ValueError) as excinfo:
+      inv.CancelProFormaInvoice()
+    assert "Only pro forma invoices can be canceled" in str(excinfo)
 
   def test_invoice_with_products(self, connection, simple_invoice_dict):
     inv = invoice.Invoice.Create(connection, simple_invoice_dict)
