8be5b54ad61938ccff45fe5df28a1469060aa4df
stefvanhouten <stefvanhouten@gmail.com>
2022-05-21 10:17:18 +0200

    Update schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -204,6 +204,20 @@ CREATE TABLE `paymentPlatform` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `proFormaSequenceTable`
+--
+
+DROP TABLE IF EXISTS `proFormaSequenceTable`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `proFormaSequenceTable` (
+  `ID` smallint NOT NULL AUTO_INCREMENT,
+  `sequenceNumber` varchar(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `user`
 --
 
@@ -282,4 +296,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-18 14:53:28
+-- Dump completed on 2022-05-21 10:09:26
