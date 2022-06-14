25c8ff60ead5a67ce5f2595e397742e178cb7382
stefvanhouten <stefvanhouten@gmail.com>
2022-06-14 15:56:46 +0200

    Now extracts the secret from redirect/notification

--- invoices/mollie/urls.py
+++ invoices/mollie/urls.py
@@ -4,9 +4,12 @@ API_VERSION = "/api/v1"
 
 urls = [
     ## Mollie routes
-    (f"{API_VERSION}/mollie/redirect/(\d+)", (mollie.PageMaker, "Mollie_Redirect")),
     (
-        f"{API_VERSION}/mollie/notification/([\w\-\.]+)",
+        f"{API_VERSION}/mollie/redirect/(\d+)/([\w\-\.]+)",
+        (mollie.PageMaker, "Mollie_Redirect"),
+    ),
+    (
+        f"{API_VERSION}/mollie/notification/([\w\-\.]+)/([\w\-\.]+)",
         (mollie.PageMaker, "_Mollie_HookPaymentReturn"),
     ),
 ]
