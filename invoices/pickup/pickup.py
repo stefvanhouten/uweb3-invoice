dcbe156c5fd6388511ff9636d7d2331f7b75552d
stefvanhouten <stefvanhouten@gmail.com>
2022-06-15 10:48:48 +0200

    Add logic for deleting an appointment

--- invoices/pickup/pickup.py
+++ invoices/pickup/pickup.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import uweb3
 
@@ -12,6 +13,7 @@ class PageMaker(basepages.PageMaker):
     TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), "templates")
 
     @loggedin
+    @uweb3.decorators.checkxsrf
     @uweb3.decorators.TemplateParser("slots.html")
     def RequestPickupSlots(self):
         pickup_slot_form = forms.PickupSlotForm(self.post)
@@ -26,19 +28,46 @@ class PageMaker(basepages.PageMaker):
         )
 
     @loggedin
+    @uweb3.decorators.checkxsrf
     @uweb3.decorators.TemplateParser("manage_appointments.html")
     def RequestManagePickupSlot(self, slotID):
         slot = model.Pickupslot.FromPrimary(self.connection, slotID)
         appointments = model.PickupSlotAppointment.List(
-            self.connection, conditions=f"ID={slotID}"
+            self.connection, conditions=f"pickupslot={slotID}"
         )
+
         clients = client_model.Client.List(self.connection)
         appointment_form = forms.PickupSlotAppointmentForm(self.post)
+        appointment_form.pickupslot.data = slotID
         appointment_form.client.choices = [
             (c["clientNumber"], c["name"]) for c in clients
         ]
-        appointment_form.pickupslot.data = slotID
+
+        if self.post and appointment_form.validate():
+            model.PickupSlotAppointment.Create(self.connection, appointment_form.data)
+            return uweb3.Redirect(f'/pickupslot/{slot["ID"]}', httpcode=303)
+
+        slot["start_time"] = datetime.min + slot["start_time"]
+        slot["end_time"] = datetime.min + slot["end_time"]
+        pickup_slot_form = forms.PickupSlotForm(data=slot)
 
         return dict(
-            slot=slot, appointments=appointments, appointment_form=appointment_form
+            slot=slot,
+            appointments=list(appointments),
+            appointment_form=appointment_form,
+            pickup_slot_form=pickup_slot_form,
         )
+
+    @loggedin
+    @uweb3.decorators.checkxsrf
+    def RequestDeleteAppointment(self, slotID, appointmentID):
+        slot = model.Pickupslot.FromPrimary(self.connection, slotID)
+        appointment = model.PickupSlotAppointment.FromPrimary(
+            self.connection, appointmentID
+        )
+
+        if appointment["pickupslot"]["ID"] != slot["ID"]:
+            return self.Error("Appointment for this slot not found", httpcode=404)
+
+        appointment.Delete()
+        return uweb3.Redirect(f'/pickupslot/{slot["ID"]}', httpcode=303)
--- invoices/pickup/urls.py
+++ invoices/pickup/urls.py
@@ -3,4 +3,8 @@ from invoices.pickup.pickup import PageMaker
 urls = [
     ("/pickupslots", (PageMaker, "RequestPickupSlots")),
     ("/pickupslot/(\d+)", (PageMaker, "RequestManagePickupSlot")),
+    (
+        "/pickupslot/(\d+)/appointment/(\d+)/delete",
+        (PageMaker, "RequestDeleteAppointment"),
+    ),
 ]
