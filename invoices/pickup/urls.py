f53ae1e7bac59f1fa542777aed0be0dc56719af3
stefvanhouten <stefvanhouten@gmail.com>
2022-06-15 14:08:00 +0200

    Add route to request an appointment

--- invoices/pickup/urls.py
+++ invoices/pickup/urls.py
@@ -3,6 +3,7 @@ from invoices.pickup.pickup import PageMaker
 urls = [
     ("/pickupslots", (PageMaker, "RequestPickupSlots")),
     ("/pickupslot/(\d+)", (PageMaker, "RequestManagePickupSlot")),
+    ("/pickupslot/(\d+)/appointment/(\d+)", (PageMaker, "RequestAppointment")),
     (
         "/pickupslot/(\d+)/appointment/(\d+)/delete",
         (PageMaker, "RequestDeleteAppointment"),
